726ed47368d3e26cc1663b27233f5b58ee5bbdaf
Muneeb Mughal <muneeb.mughal@abendsoft.com>
2023-07-13 11:14:47 -0700

    set routing and set custom theme

---
+++ src/theme/index.jsx
@@ -0,0 +1,22 @@
+import { useMemo } from "react";
+import { createTheme, ThemeProvider, StyledEngineProvider } from '@mui/material/styles';
+import { palette } from './palette';
+import CssBaseline from '@mui/material/CssBaseline';
+
+// eslint-disable-next-line react/prop-types
+export const CustomTheme = ({ children }) => {
+  const themeOptions = useMemo(() => ({
+    palette,
+  }), []);
+
+  const theme = createTheme(themeOptions);
+
+  return (
+    <StyledEngineProvider injectFirst>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </StyledEngineProvider>
+  );
+};
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+
+  return {
+    plugins: [react()],
+    resolve: {
+      alias: [
+        {
+          find: '@src/',
+          replacement: `/${isProduction ? 'src' : 'src'}/`,
+        },
+      ],
+    },
+    server: {
+      open: true,
+    },
+  };
+});
